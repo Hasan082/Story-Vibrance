606465f466778c5dbf5f288198f3465a0ae179f2
Md Hasanuzzaman <dr.has82@gmail.com>
2024-02-09 06:14:21 +0600

    Register UI Done

--- app/src/main/java/com/hasan/storyvibrance/auth/WelcomeActivity.java
+++ app/src/main/java/com/hasan/storyvibrance/auth/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.hasan.storyvibrance.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,28 @@ import com.hasan.storyvibrance.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    Button register, login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+
+        // Set OnClickListener for the Register button
+        register.setOnClickListener(v -> {
+            Intent intent = new Intent(WelcomeActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+
+        // Set OnClickListener for the Login button
+        login.setOnClickListener(v -> {
+            Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+
+
     }
 }
