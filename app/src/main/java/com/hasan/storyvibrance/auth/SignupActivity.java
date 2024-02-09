572b7c640a42ce164b92a07998a33ecbb94c7f65
Md Hasanuzzaman <dr.has82@gmail.com>
2024-02-09 17:52:27 +0600

    Color contrast fixed

--- app/src/main/java/com/hasan/storyvibrance/auth/SignupActivity.java
+++ app/src/main/java/com/hasan/storyvibrance/auth/SignupActivity.java
@@ -1,16 +1,27 @@
 package com.hasan.storyvibrance.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.hasan.storyvibrance.R;
 
 public class SignupActivity extends AppCompatActivity {
 
+    Button signup;
+    TextView loginTextBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        loginTextBtn = findViewById(R.id.loginTextBtn);
+
+        loginTextBtn.setOnClickListener(v -> {
+            startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+        });
+
     }
 }
