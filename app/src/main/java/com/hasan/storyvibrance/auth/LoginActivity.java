9bef5bc9a595f08739d3b8fb7d3c050a61c330e1
Md Hasanuzzaman <dr.has82@gmail.com>
2024-04-05 18:02:48 +0600

    SIgnUp page done with firebase and Data Binding

--- app/src/main/java/com/hasan/storyvibrance/auth/LoginActivity.java
+++ app/src/main/java/com/hasan/storyvibrance/auth/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.hasan.storyvibrance.auth;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +16,7 @@ public class LoginActivity extends AppCompatActivity {
 
     Button login;
     TextView signTxtBtn;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +25,13 @@ public class LoginActivity extends AppCompatActivity {
         login = findViewById(R.id.login);
         signTxtBtn = findViewById(R.id.signTxtBtn);
 
-        login.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, BottomNavActivity.class)));
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                if
+                startActivity(new Intent(LoginActivity.this, BottomNavActivity.class));
+            }
+        });
         signTxtBtn.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, SignupActivity.class)));
 
     }
--- app/src/main/java/com/hasan/storyvibrance/auth/SignupActivity.java
+++ app/src/main/java/com/hasan/storyvibrance/auth/SignupActivity.java
@@ -2,26 +2,73 @@ package com.hasan.storyvibrance.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.hasan.storyvibrance.R;
+import com.hasan.storyvibrance.databinding.ActivitySignupBinding;
+
+import java.util.Objects;
 
 public class SignupActivity extends AppCompatActivity {
 
-    Button signup;
-    TextView loginTextBtn;
+    private FirebaseAuth mAuth; // Firebase authentication instance
+
+    ActivitySignupBinding binding; // View binding object
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
-        signup = findViewById(R.id.signup);
-        loginTextBtn = findViewById(R.id.loginTextBtn);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signup); // Set content view using data binding
+        mAuth = FirebaseAuth.getInstance(); // Initialize Firebase authentication instance
 
+        // REGISTRATION PROCESS WITH FIREBASE ===============
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Retrieve input values from edittext
+                String name = binding.nameEdTxt.getText().toString().trim();
+                String username = binding.usernameEdTxt.getText().toString().trim();
+                String password = binding.passEdTxt.getText().toString();
 
-        signup.setOnClickListener(v -> startActivity(new Intent(SignupActivity.this, LoginActivity.class)));
-        loginTextBtn.setOnClickListener(v -> startActivity(new Intent(SignupActivity.this, LoginActivity.class)));
+                // Check if input fields are not empty
+                if (!name.isEmpty() && !username.isEmpty() && !password.isEmpty()) {
+                    // Create user account with email and password
+                    mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // Start LoginActivity if all fields are filled
+                            startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                            // Registration success, show toast message
+                            Toast.makeText(SignupActivity.this, "SignUp Success! Please login!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Registration failed, retrieve error message from task
+                            String errorMessage = Objects.requireNonNull(task.getException()).getMessage();
+                            // Registration failed, show error toast message
+                            Toast.makeText(SignupActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    // Show toast message if any field is empty
+                    Toast.makeText(SignupActivity.this, "Username and/or password is empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // GO TO LOGIN PAGE IF ALREADY USER================================
+        binding.loginTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
+
+
 }
