45c2ce4dc5b3f0e9e2e2cc87e341c72c475ec09b
Md Hasanuzzaman <dr.has82@gmail.com>
2024-04-06 21:04:07 +0600

    Forget Pasword done

--- app/src/main/java/com/hasan/storyvibrance/auth/ForgetPassActivity.java
+++ app/src/main/java/com/hasan/storyvibrance/auth/ForgetPassActivity.java
@@ -1,16 +1,59 @@
 package com.hasan.storyvibrance.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.hasan.storyvibrance.R;
+import com.hasan.storyvibrance.databinding.ActivityForgetPassBinding;
 
 public class ForgetPassActivity extends AppCompatActivity {
 
+    ActivityForgetPassBinding binding;
+
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forget_pass);
-    }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_forget_pass);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        binding.sendEmail.setOnClickListener(v -> {
+            String email = binding.emailEdTxt.getText().toString().trim();
+            if (!email.isEmpty()) {
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnSuccessListener(unused -> {
+                            String feedback = "A password reset link has been sent to your registered email.";
+                            Toast.makeText(ForgetPassActivity.this, feedback, Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(ForgetPassActivity.this, LoginActivity.class));
+                            binding.emailEdTxt.setText("");
+                            finish();
+                        })
+                        .addOnFailureListener(e -> {
+                            String feedback;
+                            if (e instanceof FirebaseAuthInvalidUserException) {
+                                feedback = "No account is registered with this email.";
+                            } else {
+                                feedback = e.getMessage();
+                            }
+                            Toast.makeText(ForgetPassActivity.this, feedback, Toast.LENGTH_SHORT).show();
+                        });
+            } else {
+                String fieldErr = "Your email field is empty.";
+                Toast.makeText(ForgetPassActivity.this, fieldErr, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }//on create method==============
+
+
+
+
+
 }
