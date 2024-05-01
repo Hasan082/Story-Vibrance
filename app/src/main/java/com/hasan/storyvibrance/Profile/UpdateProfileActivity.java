4bb90dd8855fc7b5d571b113f2f4f3fa5276b086
Md Hasanuzzaman <dr.has82@gmail.com>
2024-05-02 04:00:41 +0600

    Update profile to back to profile done

--- app/src/main/java/com/hasan/storyvibrance/Profile/UpdateProfileActivity.java
+++ app/src/main/java/com/hasan/storyvibrance/Profile/UpdateProfileActivity.java
@@ -4,13 +4,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.preference.PreferenceManager;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.hasan.storyvibrance.R;
@@ -34,33 +31,32 @@ public class UpdateProfileActivity extends AppCompatActivity {
 
 
         //GET THE FULL NAME FROM DATABASE AND SET IT
-        db.collection("userdata").document(userName).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    if (documentSnapshot != null && documentSnapshot.exists()) {
-                        String personName = documentSnapshot.getString("name");
-                        // Set personName directly
+        db.collection("userdata").document(userName).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot documentSnapshot = task.getResult();
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    String personName = documentSnapshot.getString("name");
+                    if (personName != null) {
                         binding.setFullName(personName);
-                        //SET THE USER NAME AND NON EDITABLE
-                        binding.setEmailAddress(userName);
-                        //Hide loader======
+                    }  // Handle the case where personName is null
+                    binding.setEmailAddress(userName);
+                    //Hide loader======
 //                        binding.spinner.setVisibility(View.GONE);
-                    } else {
-                        //if no data available in database
-                        Toast.makeText(UpdateProfileActivity.this, "Data Not available", Toast.LENGTH_SHORT).show();
-                    }
                 } else {
-                    // Handle failures
-                    Toast.makeText(UpdateProfileActivity.this, "Database Error!", Toast.LENGTH_SHORT).show();
+                    //if no data available in database
+                    Toast.makeText(UpdateProfileActivity.this, "Data Not available", Toast.LENGTH_SHORT).show();
                 }
+            } else {
+                // Handle failures
+                Toast.makeText(UpdateProfileActivity.this, "Database Error!", Toast.LENGTH_SHORT).show();
             }
         });
 
 
-
-
+        //Back to profile page======================
+        binding.backBtn.setOnClickListener(v->{
+            getOnBackPressedDispatcher().onBackPressed();
+        });
 
 
     }//END OF ON CREATE===============
