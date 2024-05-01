72e65084ea7f8cf2bcbc557792640371e6d57743
Md Hasanuzzaman <dr.has82@gmail.com>
2024-05-02 02:21:37 +0600

    Update profile UI done and Data binding Implement

--- app/src/main/java/com/hasan/storyvibrance/Profile/UpdateProfileActivity.java
+++ app/src/main/java/com/hasan/storyvibrance/Profile/UpdateProfileActivity.java
@@ -1,26 +1,27 @@
 package com.hasan.storyvibrance.Profile;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.hasan.storyvibrance.R;
+import com.hasan.storyvibrance.databinding.ActivityUpdateProfileBinding;
+
 
 public class UpdateProfileActivity extends AppCompatActivity {
+
+    //    ActivityUpdateProfileBinding binding;
+    ActivityUpdateProfileBinding binding;
+    SharedPreferences sPref;
+    SharedPreferences.Editor sPrefEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_update_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        setContentView(R.layout.activity_update_profile);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_update_profile);
     }
+
 }
